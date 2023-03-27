a42858c55c5459f8560b5df4ca1256d15c28deac
peritissimus <kush@dubverse.ai>
2023-03-27 18:03:51 +0530

    ...

--- nvim/lua/peritissimus/plugins/nvim-cmp.lua
+++ nvim/lua/peritissimus/plugins/nvim-cmp.lua
@@ -39,7 +39,7 @@ cmp.setup({
 	-- sources for autocompletion
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" }, -- lsp
-		{ name = "luasnip" }, -- snippets
+		-- { name = "luasnip" }, -- snippets
 		{ name = "buffer" }, -- text within current buffer
 		{ name = "path" }, -- file system paths
 	}),

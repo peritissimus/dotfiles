e76673dbacebadf776797d07a44de5074b6e7ec7
Kushal Patankar <kush@dubverse.ai>
2023-07-29 17:51:04 +0530

    feat(nvim): updating for 0.9.0

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,22 @@
-require("peritissimus.core.colorscheme")
-require("peritissimus.core.keymaps")
-require("peritissimus.core.options")
-require("peritissimus.plugins-setup")
-require("peritissimus.plugins.comment")
-require("peritissimus.plugins.nvim-tree")
-require("peritissimus.plugins.lualine")
-require("peritissimus.plugins.telescope")
-require("peritissimus.plugins.gitsigns")
-require("peritissimus.plugins.nvim-cmp")
-require("peritissimus.plugins.autopairs")
-require("peritissimus.plugins.treesitter")
-require("peritissimus.plugins.lsp.mason")
-require("peritissimus.plugins.lsp.null-ls")
-require("peritissimus.plugins.lsp.lspsaga")
-require("peritissimus.plugins.lsp.lspconfig")
+require('peritissimus.base')
+require('peritissimus.highlights')
+require('peritissimus.maps')
+require('peritissimus.plugins')
+
+local has = vim.fn.has
+local is_mac = has "macunix"
+local is_win = has "win32"
+local is_wsl = has "wsl"
+
+if is_mac == 1 then
+  require('peritissimus.macos')
+end
+if is_win == 1 then
+  require('peritissimus.windows')
+end
+if is_wsl == 1 then
+  require('peritissimus.wsl')
+end
+
+local status, _ = pcall(require, "gruvbox-material")
+if (not status) then return end

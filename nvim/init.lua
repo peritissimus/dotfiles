731f89ce836468e94a8123ef2b6377a7625a05c5
Kushal Patankar <kush@dubverse.ai>
2023-12-11 15:37:23 +0530

    update: updating nvim setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,22 +1,10 @@
-require('peritissimus.base')
-require('peritissimus.highlights')
-require('peritissimus.maps')
-require('peritissimus.plugins')
-
-local has = vim.fn.has
-local is_mac = has "macunix"
-local is_win = has "win32"
-local is_wsl = has "wsl"
-
-if is_mac == 1 then
-  require('peritissimus.macos')
-end
-if is_win == 1 then
-  require('peritissimus.windows')
-end
-if is_wsl == 1 then
-  require('peritissimus.wsl')
+if vim.loader then
+	vim.loader.enable()
 end
 
-local status, _ = pcall(require, "gruvbox-material")
-if (not status) then return end
+_G.dd = function(...)
+	require("util.debug").dump(...)
+end
+vim.print = _G.dd
+
+require("config.lazy")

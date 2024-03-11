77ccef8cb6e3c76835286b0917aa975a3cf07238
peritissimus <149.kush@gmail.com>
2024-03-11 10:51:34 +0530

    ...

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,12 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 
 alias startenv=". .venv/bin/activate.fish"
 alias lz="lazygit"
+alias mux="tmuxinator"
+
+set -gx EDITOR nvim
+
+set -x GOPATH $HOME/go
+set -x PATH $GOPATH/bin $PATH
 
 if status is-interactive
     # Commands to run in interactive sessions can go here

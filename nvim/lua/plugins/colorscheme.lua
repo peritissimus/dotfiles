28cf1a664eb117bae4e9649484a204ddf45317eb
peritissimus <149.kush@gmail.com>
2024-08-19 23:31:39 +0530

    update: colosheme tokyonight

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,27 @@
 return {
+	-- {
+	-- 	"tjdevries/colorbuddy.vim",
+	-- },
+	-- {
+	-- 	"peritissimus/gruvbox.vim",
+	-- 	lazy = true,
+	-- 	priority = 1000,
+	-- 	opts = function()
+	-- 		return {
+	-- 			transparent = true,
+	-- 		}
+	-- 	end,
+	-- },
 	{
-		"tjdevries/colorbuddy.vim",
-	},
-	{
-		"peritissimus/gruvbox.vim",
-		lazy = true,
-		priority = 1000,
-		opts = function()
-			return {
-				transparent = true,
-			}
-		end,
+		"folke/tokyonight.nvim",
+		lazy = false,
+		opts = {
+			style = "moon",
+			transparent = true,
+			styles = {
+				sibdebars = "transparent",
+				floats = "transparent",
+			},
+		},
 	},
 }

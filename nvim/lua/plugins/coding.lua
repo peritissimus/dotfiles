7ad3c691fdb56b80aef6a7933ad18169bd66b43c
peritissimus <149.kush@gmail.com>
2024-11-17 21:05:45 +0530

    update(nvim): remove flutter tools

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,12 +1,5 @@
 return {
 	{
-		"akinsho/flutter-tools.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("flutter-tools").setup({})
-		end,
-	},
-	{
 		"brenoprata10/nvim-highlight-colors",
 		config = function()
 			require("nvim-highlight-colors").setup({

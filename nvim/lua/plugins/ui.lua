87aaccfc527f170abee75a865912e030e5182a57
peritissimus <149.kush@gmail.com>
2024-11-19 13:50:13 +0530

    update: dashboard plugin

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -83,35 +83,34 @@ return {
 		},
 	},
 
-	-- {
-	-- 	"folke/zen-mode.nvim",
-	-- 	cmd = "ZenMode",
-	-- 	opts = {
-	-- 		plugins = {
-	-- 			gitsigns = true,
-	-- 			tmux = true,
-	-- 			kitty = { enabled = false, font = "+2" },
-	-- 		},
-	-- 	},
-	-- 	keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
-	-- },
-
 	{
-		"nvimdev/dashboard-nvim",
-		event = "VimEnter",
-		opts = function(_, opts)
-			local logo = [[
-  ██████╗ ███████╗██████╗ ██╗████████╗██╗███████╗███████╗██╗███╗   ███╗██╗   ██╗███████╗
-  ██╔══██╗██╔════╝██╔══██╗██║╚══██╔══╝██║██╔════╝██╔════╝██║████╗ ████║██║   ██║██╔════╝
-  ██████╔╝█████╗  ██████╔╝██║   ██║   ██║███████╗███████╗██║██╔████╔██║██║   ██║███████╗
-  ██╔═══╝ ██╔══╝  ██╔══██╗██║   ██║   ██║╚════██║╚════██║██║██║╚██╔╝██║██║   ██║╚════██║
-  ██║     ███████╗██║  ██║██║   ██║   ██║███████║███████║██║██║ ╚═╝ ██║╚██████╔╝███████║
-  ╚═╝     ╚══════╝╚═╝  ╚═╝╚═╝   ╚═╝   ╚═╝╚══════╝╚══════╝╚═╝╚═╝     ╚═╝ ╚═════╝ ╚══════╝
-                                                                                        
-      ]]
-
-			logo = string.rep("\n", 8) .. logo .. "\n\n"
-			opts.config.header = vim.split(logo, "\n")
-		end,
+		"folke/snacks.nvim",
+		opts = {
+			dashboard = {
+				preset = {
+					header = [[
+██████╗ ███████╗██████╗ ██╗████████╗██╗███████╗███████╗██╗███╗   ███╗██╗   ██╗███████╗
+██╔══██╗██╔════╝██╔══██╗██║╚══██╔══╝██║██╔════╝██╔════╝██║████╗ ████║██║   ██║██╔════╝
+██████╔╝█████╗  ██████╔╝██║   ██║   ██║███████╗███████╗██║██╔████╔██║██║   ██║███████╗
+██╔═══╝ ██╔══╝  ██╔══██╗██║   ██║   ██║╚════██║╚════██║██║██║╚██╔╝██║██║   ██║╚════██║
+██║     ███████╗██║  ██║██║   ██║   ██║███████║███████║██║██║ ╚═╝ ██║╚██████╔╝███████║
+╚═╝     ╚══════╝╚═╝  ╚═╝╚═╝   ╚═╝   ╚═╝╚══════╝╚══════╝╚═╝╚═╝     ╚═╝ ╚═════╝ ╚══════╝
+ ]],
+        -- stylua: ignore
+        ---@type snacks.dashboard.Item[]
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+				},
+			},
+		},
 	},
 }

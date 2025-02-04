e6dba8a5fd6cd12aaf1deff5abe933b317c9e52e
Kushal Patankar <149.kush@gmail.com>
2025-02-04 21:21:17 +0530

    refactor(lsp, treesitter): clean up and format configuration files for LSP and Treesitter plugins

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,173 +1,177 @@
 return {
-	-- tools
-	{
-		"williamboman/mason.nvim",
-		opts = function(_, opts)
-			vim.list_extend(opts.ensure_installed, {
-				"prettier",
-				"prettierd",
-				"stylua",
-				"selene",
-				"luacheck",
-				"shellcheck",
-				"shfmt",
-				"sqlfluff",
-				"tailwindcss-language-server",
-				"typescript-language-server",
-				"css-lsp",
-				"ruff-lsp",
-				"basedpyright",
-			})
-		end,
-	},
+  -- tools
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "prettier",
+        "prettierd",
+        "stylua",
+        "selene",
+        "luacheck",
+        "shellcheck",
+        "shfmt",
+        "sqlfluff",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+        "css-lsp",
+        "ruff-lsp",
+        "basedpyright",
+      })
+    end,
+  },
 
-	{
-		"pmizio/typescript-tools.nvim",
-		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-		opts = {
-			settings = {
-				tsserver_max_memory = "8192MB",
-				separate_diagnostic_server = true,
-				publish_diagnostic_on = "insert_leave",
-				expose_as_code_action = "all",
-				filter_out_diagnostics_by_code = { 80001 },
-			},
-			on_attach = function(_, bufnr)
-				local function buf_set_keymap(...)
-					vim.api.nvim_buf_set_keymap(bufnr, ...)
-				end
-				local opts = { noremap = true, silent = true }
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {
+      settings = {
+        tsserver_max_memory = "8192MB",
+        separate_diagnostic_server = true,
+        publish_diagnostic_on = "insert_leave",
+        expose_as_code_action = "all",
+        filter_out_diagnostics_by_code = { 80001 },
+      },
+      on_attach = function(_, bufnr)
+        local function buf_set_keymap(...)
+          vim.api.nvim_buf_set_keymap(bufnr, ...)
+        end
+        local opts = { noremap = true, silent = true }
 
-				-- Go to Source Definition
-				buf_set_keymap("n", "gD", "<cmd>TSToolsGoToSourceDefinition<CR>", opts)
+        -- Go to Source Definition
+        buf_set_keymap("n", "gD", "<cmd>TSToolsGoToSourceDefinition<CR>", opts)
 
-				-- Find All File References
-				buf_set_keymap("n", "gR", "<cmd>TSToolsFileReferences<CR>", opts)
+        -- Find All File References
+        buf_set_keymap("n", "gR", "<cmd>TSToolsFileReferences<CR>", opts)
 
-				-- Organize Imports
-				buf_set_keymap("n", "<leader>co", "<cmd>TSToolsOrganizeImports<CR>", opts)
+        -- Organize Imports
+        buf_set_keymap("n", "<leader>co", "<cmd>TSToolsOrganizeImports<CR>", opts)
 
-				-- Add Missing Imports
-				buf_set_keymap("n", "<leader>cM", "<cmd>TSToolsAddMissingImports<CR>", opts)
+        -- Add Missing Imports
+        buf_set_keymap("n", "<leader>cM", "<cmd>TSToolsAddMissingImports<CR>", opts)
 
-				-- Remove Unused Imports
-				buf_set_keymap("n", "<leader>cu", "<cmd>TSToolsRemoveUnusedImports<CR>", opts)
+        -- Remove Unused Imports
+        buf_set_keymap("n", "<leader>cu", "<cmd>TSToolsRemoveUnusedImports<CR>", opts)
 
-				-- Fix All Diagnostics
-				buf_set_keymap("n", "<leader>cD", "<cmd>TSToolsFixAll<CR>", opts)
-			end,
-		},
-	},
+        -- Fix All Diagnostics
+        buf_set_keymap("n", "<leader>cD", "<cmd>TSToolsFixAll<CR>", opts)
+      end,
+    },
+  },
 
-	{
-		"neovim/nvim-lspconfig",
-		opts = {
-			inlay_hints = { enabled = false },
-			diagnostics = {
-				underline = true,
-				update_in_insert = false,
-				virtual_text = {
-					spacing = 2,
-					source = "if_many",
-					prefix = "●",
-				},
-				severity_sort = true,
-			},
-			servers = {
-				vtsls = {
-					enabled = false,
-					filetypes = {
-						"javascript",
-						"javascriptreact",
-						"typescript",
-						"typescriptreact",
-					},
-					root_dir = require("lspconfig").util.root_pattern("nx.json", "package.json"),
-					settings = {
-						typescript = {
-							maxTsServerMemory = 4096, -- Example setting to increase memory
-						},
-						javascript = {
-							-- Merged settings as per setup function
-						},
-					},
-					on_attach = function(client, buffer)
-						-- Simplified on_attach if necessary
-					end,
-					-- Lazy loading flag (if supported by your plugin manager)
-					lazy = true,
-				},
-				ruff_lsp = {
-					enabled = true,
-					settings = {},
-					root_dir = require("lspconfig").util.root_pattern("nx.json", ".git"),
-				},
-				pyright = {
-					enabled = true,
-					root_dir = require("lspconfig").util.root_pattern("nx.json", ".git"),
-					settings = {
-						python = {
-							analysis = {
-								typeCheckingMode = "basic",
-								autoSearchPaths = true,
-								useLibraryCodeForTypes = true,
-							},
-						},
-					},
-				},
-				lua_ls = {
-					single_file_support = true,
-					root_dir = require("lspconfig").util.root_pattern("nx.json", ".git"),
-					settings = {
-						Lua = {
-							workspace = {
-								checkThirdParty = false,
-							},
-							completion = {
-								callSnippet = "Replace",
-							},
-							diagnostics = {
-								globals = { "vim" },
-							},
-							hint = {
-								enable = true,
-								arrayIndex = "Enable",
-								setType = true,
-								paramName = "All",
-								paramType = true,
-								semicolon = "All",
-							},
-						},
-					},
-				},
-				eslint = {
-					root_dir = require("lspconfig").util.root_pattern("nx.json", "package.json"),
-					settings = {
-						workingDirectory = { mode = "auto" },
-					},
-				},
-				-- Disable unused servers
-				basedpyright = { enabled = false },
-				cssls = { enabled = false },
-				tailwindcss = { enabled = false },
-			},
-			setup = {
-				vtsls = function(_, opts)
-					-- Ensure merged settings
-					opts.settings.javascript =
-						vim.tbl_deep_extend("force", {}, opts.settings.typescript, opts.settings.javascript or {})
-					-- Optional: Further optimize or remove custom commands
-				end,
-				eslint = function()
-					require("lazyvim.util").lsp.on_attach(function(client)
-						if client.name == "eslint" then
-							client.server_capabilities.documentFormattingProvider = true
-						elseif client.name == "tsserver" then
-							client.server_capabilities.documentFormattingProvider = false
-						end
-					end)
-				end,
-			},
-		},
-	},
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+      diagnostics = {
+        underline = true,
+        update_in_insert = false,
+        virtual_text = {
+          spacing = 2,
+          source = "if_many",
+          prefix = "●",
+        },
+        severity_sort = true,
+      },
+      servers = {
+        terraformls = {},
+        bash_ls = {
+          filetypes = { "sh", "zsh", "fish" }
+        },
+        vtsls = {
+          enabled = false,
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "typescript",
+            "typescriptreact",
+          },
+          root_dir = require("lspconfig").util.root_pattern("nx.json", "package.json"),
+          settings = {
+            typescript = {
+              maxTsServerMemory = 4096, -- Example setting to increase memory
+            },
+            javascript = {
+              -- Merged settings as per setup function
+            },
+          },
+          on_attach = function(client, buffer)
+            -- Simplified on_attach if necessary
+          end,
+          -- Lazy loading flag (if supported by your plugin manager)
+          lazy = true,
+        },
+        ruff_lsp = {
+          enabled = true,
+          settings = {},
+          root_dir = require("lspconfig").util.root_pattern("nx.json", ".git"),
+        },
+        pyright = {
+          enabled = true,
+          root_dir = require("lspconfig").util.root_pattern("nx.json", ".git"),
+          settings = {
+            python = {
+              analysis = {
+                typeCheckingMode = "basic",
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+              },
+            },
+          },
+        },
+        lua_ls = {
+          single_file_support = true,
+          root_dir = require("lspconfig").util.root_pattern("nx.json", ".git"),
+          settings = {
+            Lua = {
+              workspace = {
+                checkThirdParty = false,
+              },
+              completion = {
+                callSnippet = "Replace",
+              },
+              diagnostics = {
+                globals = { "vim" },
+              },
+              hint = {
+                enable = true,
+                arrayIndex = "Enable",
+                setType = true,
+                paramName = "All",
+                paramType = true,
+                semicolon = "All",
+              },
+            },
+          },
+        },
+        eslint = {
+          root_dir = require("lspconfig").util.root_pattern("nx.json", "package.json"),
+          settings = {
+            workingDirectory = { mode = "auto" },
+          },
+        },
+        -- Disable unused servers
+        basedpyright = { enabled = false },
+        cssls = { enabled = false },
+        tailwindcss = { enabled = false },
+      },
+      setup = {
+        vtsls = function(_, opts)
+          -- Ensure merged settings
+          opts.settings.javascript =
+              vim.tbl_deep_extend("force", {}, opts.settings.typescript, opts.settings.javascript or {})
+          -- Optional: Further optimize or remove custom commands
+        end,
+        eslint = function()
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
+      },
+    },
+  },
 }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,69 +1,71 @@
 return {
-	{ "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
+  { "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = {
-			ensure_installed = {
-				"cmake",
-				"cpp",
-				"css",
-				"fish",
-				"gitignore",
-				"go",
-				"graphql",
-				"http",
-				"rust",
-				"scss",
-				"sql",
-				"svelte",
-				"python",
-				"typescript",
-				"tsx",
-				"lua",
-			},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "cmake",
+        "cpp",
+        "css",
+        "fish",
+        "gitignore",
+        "go",
+        "graphql",
+        "http",
+        "rust",
+        "scss",
+        "sql",
+        "svelte",
+        "python",
+        "typescript",
+        "tsx",
+        "lua",
+        "terraform",
+        "hcl",
+      },
 
-			-- matchup = {
-			-- 	enable = true,
-			-- },
+      -- matchup = {
+      -- 	enable = true,
+      -- },
 
-			-- https://github.com/nvim-treesitter/playground#query-linter
-			query_linter = {
-				enable = true,
-				use_virtual_text = true,
-				lint_events = { "BufWrite", "CursorHold" },
-			},
+      -- https://github.com/nvim-treesitter/playground#query-linter
+      query_linter = {
+        enable = true,
+        use_virtual_text = true,
+        lint_events = { "BufWrite", "CursorHold" },
+      },
 
-			playground = {
-				enable = true,
-				disable = {},
-				updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-				persist_queries = true, -- Whether the query persists across vim sessions
-				keybindings = {
-					toggle_query_editor = "o",
-					toggle_hl_groups = "i",
-					toggle_injected_languages = "t",
-					toggle_anonymous_nodes = "a",
-					toggle_language_display = "I",
-					focus_language = "f",
-					unfocus_language = "F",
-					update = "R",
-					goto_node = "<cr>",
-					show_help = "?",
-				},
-			},
-		},
-		config = function(_, opts)
-			require("nvim-treesitter.configs").setup(opts)
-			require("nvim-treesitter.install").compilers = { "zig", "clang", "gcc" }
+      playground = {
+        enable = true,
+        disable = {},
+        updatetime = 25,    -- Debounced time for highlighting nodes in the playground from source code
+        persist_queries = true, -- Whether the query persists across vim sessions
+        keybindings = {
+          toggle_query_editor = "o",
+          toggle_hl_groups = "i",
+          toggle_injected_languages = "t",
+          toggle_anonymous_nodes = "a",
+          toggle_language_display = "I",
+          focus_language = "f",
+          unfocus_language = "F",
+          update = "R",
+          goto_node = "<cr>",
+          show_help = "?",
+        },
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+      require("nvim-treesitter.install").compilers = { "zig", "clang", "gcc" }
 
-			-- -- MDX
-			-- vim.filetype.add({
-			-- 	extension = {
-			-- 		mdx = "mdx",
-			-- 	},
-			-- })
-			-- vim.treesitter.language.register("markdown", "mdx")
-		end,
-	},
+      -- -- MDX
+      -- vim.filetype.add({
+      -- 	extension = {
+      -- 		mdx = "mdx",
+      -- 	},
+      -- })
+      -- vim.treesitter.language.register("markdown", "mdx")
+    end,
+  },
 }

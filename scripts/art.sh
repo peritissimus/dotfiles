bc514679545a475f831f9237fc855532f4980aa9
Kushal Patankar <149.kush@gmail.com>
2025-02-14 19:06:53 +0530

    feat(scripts): add stylus mapping script for Wacom devices
    * scripts/art.sh: create script to map Wacom stylus and eraser to HDMI-1
    * scripts/gcm.sh: rename commit.sh to gcm.sh
    * scripts/gpr.sh: rename pr.sh to gpr.sh

---
+++ scripts/art.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+xsetwacom set "Wacom One by Wacom S Pen stylus" MapToOutput "HDMI-1"
+xsetwacom set "Wacom One by Wacom S Pen eraser" MapToOutput "HDMI-1"
+
+echo "Stylus mapped to HDMI"

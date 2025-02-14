42a17786014fb6241571ff4d63361f22eee28ec1
Kushal Patankar <149.kush@gmail.com>
2025-02-14 19:06:06 +0530

    refactor(nvim): remove unused configuration files and scripts

--- fish/config.fish
+++ fish/config.fish
@@ -48,7 +48,7 @@ alias lz="lazygit"
 alias tf="terraform"
 alias mux="tmuxinator"
 alias c2p="code2prompt"
-alias gcm="~/scripts/commit.sh"
+alias gcm="~/dotfiles/scripts/gcm.sh"
 
 ## Git-related
 alias timelygit='GIT_SSH_COMMAND="ssh -i ~/.ssh/timely_key" git'

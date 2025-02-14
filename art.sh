be11f9585ca441a634044188ebc31d77dd88b2fc
Kushal Patankar <149.kush@gmail.com>
2025-02-14 19:06:42 +0530

    chore(vifm): remove obsolete config and script files

--- art.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-xsetwacom set "Wacom One by Wacom S Pen stylus" MapToOutput "HDMI-1"
-xsetwacom set "Wacom One by Wacom S Pen eraser" MapToOutput "HDMI-1"
-
-echo "Stylus mapped to HDMI"
--- spotify.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-LD_PRELOAD=/usr/local/lib/spotify-adblock.so spotify

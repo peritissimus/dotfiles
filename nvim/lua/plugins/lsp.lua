14fd4c1ae6ff5b8d926c754742aeefda8c33dfbc
Kushal Patankar <149.kush@gmail.com>
2025-03-17 16:54:15 +0530

    feat(nvim/plugins/lsp): increase tsserver maximum memory allocation
    Updated the tsserver_max_memory setting from 8192MB to 10000MB to enhance performance during larger TypeScript builds and improve overall responsiveness in the IDE.

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -26,7 +26,7 @@ return {
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
     opts = {
       settings = {
-        tsserver_max_memory = "8192MB",
+        tsserver_max_memory = "10000MB",
         separate_diagnostic_server = true,
         publish_diagnostic_on = "insert_leave",
         expose_as_code_action = "all",

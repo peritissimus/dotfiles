640c03e6e4a48e151b0378eda3f09d0f6b9449ff
peritissimus <149.kush@gmail.com>
2025-03-22 01:59:21 +0530

    feat(nvim/plugins): add conform.nvim plugin for code formatting
    This commit introduces the conform.nvim plugin, which provides functionality to format various programming language files within Neovim. The setup includes formatters for JavaScript, TypeScript, and Python, allowing for improved code consistency and style adherence.

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -1,62 +1,61 @@
 return {
+  {
+    "stevearc/conform.nvim",
+    dependencies = { "mason.nvim" },
+    lazy = true,
+    cmd = "ConformInfo",
+    keys = {
+      {
+        "<leader>cF",
+        function()
+          require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
+        end,
+        mode = { "n", "v" },
+        desc = "Format Injected Langs",
+      },
+    },
+    init = function()
+      -- Install the conform formatter on VeryLazy
+      LazyVim.on_very_lazy(function()
+        LazyVim.format.register({
+          name = "conform.nvim",
+          priority = 100,
+          primary = true,
+          format = function(buf)
+            require("conform").format({ bufnr = buf })
+          end,
+          sources = function(buf)
+            local ret = require("conform").list_formatters(buf)
+            ---@param v conform.FormatterInfo
+            return vim.tbl_map(function(v)
+              return v.name
+            end, ret)
+          end,
+        })
+      end)
+    end,
+    opts = function()
+      local opts = {
+        default_format_opts = {
+          timeout_ms = 3000,
+          async = false,           -- not recommended to change
+          quiet = false,           -- not recommended to change
+          lsp_format = "fallback", -- not recommended to change
+        },
+        formatters_by_ft = {
+          javascript = { "prettierd", "prettier" },
+          typescript = { "prettierd", "prettier" },
+          javascriptreact = { "prettierd", "prettier" },
+          typescriptreact = { "prettierd", "prettier" },
+          python = { "ruff_fix", "ruff_format" },
+        },
 
-	{
-		"stevearc/conform.nvim",
-		dependencies = { "mason.nvim" },
-		lazy = true,
-		cmd = "ConformInfo",
-		keys = {
-			{
-				"<leader>cF",
-				function()
-					require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
-				end,
-				mode = { "n", "v" },
-				desc = "Format Injected Langs",
-			},
-		},
-		init = function()
-			-- Install the conform formatter on VeryLazy
-			LazyVim.on_very_lazy(function()
-				LazyVim.format.register({
-					name = "conform.nvim",
-					priority = 100,
-					primary = true,
-					format = function(buf)
-						require("conform").format({ bufnr = buf })
-					end,
-					sources = function(buf)
-						local ret = require("conform").list_formatters(buf)
-						---@param v conform.FormatterInfo
-						return vim.tbl_map(function(v)
-							return v.name
-						end, ret)
-					end,
-				})
-			end)
-		end,
-		opts = function()
-			local opts = {
-				default_format_opts = {
-					timeout_ms = 3000,
-					async = false, -- not recommended to change
-					quiet = false, -- not recommended to change
-					lsp_format = "fallback", -- not recommended to change
-				},
-				formatters_by_ft = {
-					javascript = { "prettierd", "prettier" },
-					typescript = { "prettierd", "prettier" },
-					javascriptreact = { "prettierd", "prettier" },
-					typescriptreact = { "prettierd", "prettier" },
-					python = { "isort", "black" },
-				},
-
-				---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
-				formatters = {
-					injected = { options = { ignore_errors = true } },
-				},
-			}
-			return opts
-		end,
-	},
+        ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
+        formatters = {
+          injected = { options = { ignore_errors = true } },
+        },
+      }
+      return opts
+    end,
+  },
 }
